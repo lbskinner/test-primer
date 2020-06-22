a371dc757049c9d41e52e341ac213a72b1a72c71
Leila Skinner <beileiwang@gmail.com>
2020-06-22 10:52:06 -0500

    using regex and passed all three tests

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 function toTitleCase(someString) {
-  const words = someString.split(" ");
+  //   const words = someString.split(" ");
 
-  const titleCasedWords = [];
-  words.forEach((str) => {
-    titleCasedWords.push(str[0].toUpperCase() + str.substring(1));
+  //   const titleCasedWords = [];
+  //   words.forEach((str) => {
+  //     titleCasedWords.push(str[0].toUpperCase() + str.substring(1));
+  //   });
+  //   return titleCasedWords.join(" ");
+  const re = /(^|[\s-])([a-zA-z])/g;
+  const newString = someString.replace(re, (match, symbol, letter) => {
+    console.log("S: ", symbol, "L: ", letter, "M: ", match);
+
+    return symbol + letter.toUpperCase();
   });
-  return titleCasedWords.join(" ");
+  return newString;
 }
 
 module.exports = toTitleCase;
